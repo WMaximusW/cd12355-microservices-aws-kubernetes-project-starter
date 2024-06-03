8d16eea6d31db106db127efb33563650823ebe31
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-06-03 18:47:28 +0700

    UPDATE

--- analytics/config.py
+++ analytics/config.py
@@ -4,11 +4,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
-db_host = os.environ.get("DB_HOST", "127.0.0.1")
-db_port = os.environ.get("DB_PORT", "5432")
-db_name = os.environ.get("DB_NAME", "postgres")
+db_username = os.environ.get("DB_USERNAME", "trungnq72-user")
+db_password = os.environ.get("DB_PASSWORD", "trungnq72-password")
+db_host = os.environ.get("DB_HOST", "host.docker.internal")
+db_port = os.environ.get("DB_PORT", "5433")
+db_name = os.environ.get("DB_NAME", "trungnq72-database")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
